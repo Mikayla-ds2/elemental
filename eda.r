ef5c02e145d759b37b234fc94fa79cc0d555a6d5
Mikayla Solomon <mikaylasolomon68@gmail.com>
2026-01-10 18:29:04 -0500

    feature engineering

--- eda.r
+++ eda.r
@@ -22,3 +22,25 @@ library(rjson) # was going to use this but the glimpse and head looked superrrr 
 elements <- read.csv('elements.csv')
 glimpse(elements)
 
+dim(elements)
+summary(elements)
+
+# dropping first index column + electron Configuration dictionary
+elements <- subset(elements, select = -c(X, electronConfiguration))
+dim(elements)
+summary(elements)
+
+# saw that year Discovered column was character type but forgot the Ancient
+elements$yearDiscovered <- as.numeric(elements$yearDiscovered)
+summary(elements)
+
+print(sum(is.na(elements)))
+
+# all rows that elements were discovered in ancient times
+missingValueRows <- elements[!complete.cases(elements), ]
+print(missingValueRows)
+
+# now feature engineering an ancient column
+elements$isAncient <- is.na(elements$yearDiscovered)
+elements$yearDiscovered[is.na(elements$yearDiscovered)] <- -3000
+
